23d1cad975eef1b21a9553c683816b96007edc5a
Aaradhya0707 <agrawal.Aaradhya0707@gmail.com>
2020-03-17 18:26:55 +0530

    Final Changes

--- FlaskRunner.py
+++ FlaskRunner.py
@@ -3,99 +3,164 @@ from flask import request
 from flask_pymongo import PyMongo  
 import json
 from Utils import *
+from bson.objectid import ObjectId
 # Flask constructor takes the name of  
 # current module (__name__) as argument. 
 app = Flask(__name__) 
 app.config["MONGO_URI"]="mongodb://localhost:27017/Assignment"
 mongo=PyMongo(app)  
 
-
-@app.route('/average',methods=["GET","POST"])  
+#defining end point for the api
+@app.route('/',methods=["POST"])  
 def nap_products_with_discount_greater_than_n_percent():
-    list=[]
+    #creating list for the required query
+    listProds=[] 
+    brand_set={""}
+    brand_set.remove("")
+    disc_set={""}
+    disc_set.remove("")
+    # creating dict for the output
     dict={}
+    #checking the request type
     if request.method=="POST":
         jsonRequest=request.json#returns a dictionary
-        queryType=jsonRequest['query_type']
-        if queryType=='discounted_products_list':
-            listFilter=jsonRequest['filters']# returns a list
-            for x in range(len(listFilter)):
-                operand1=listFilter[x]['operand1']
-                operand2=listFilter[x]['operand2']
-                operator=listFilter[x]['operator']
-                if operand1 == 'discount':    
-                    cursor=mongo.db.Dataset.find({'$where':'this.price.regular_price.value > this.price.offer_price.value'},{'_id':1,'price.regular_price.value':1,'price.offer_price.value':1})
-                    a,b,list=Utils.parseCursorForAverageDiscAndCount(cursor,operator,operand2)
-                    cursor.close
-                if operand1=='brand.name' and operator=='==':
-                    cursor=mongo.db.Dataset.find({operand1:operand2})
-                    for doc in cursor:
-                        regularPrice=doc['price']['regular_price']['value']
-                        offerPrice=doc['price']['offer_price']['value']
-                        if offerPrice<regularPrice:
-                            list.append(str(doc['_id']))
-                    cursor.close
-            dict['discounted_products_list']=list
-            return(str(dict))
-        if queryType=='discounted_products_count|avg_discount':
-            count=0
-            totalDiscount=0.0
-            listFilter=jsonRequest['filters']# returns a list
-            for x in range(len(listFilter)):
-                operand1=listFilter[x]['operand1']
-                operand2=listFilter[x]['operand2']
-                operator=listFilter[x]['operator']
-                if operand1 == 'discount':    
-                    cursor=mongo.db.Dataset.find({'$where':'this.price.regular_price.value > this.price.offer_price.value'},{'_id':1,'price.regular_price.value':1,'price.offer_price.value':1})
-                    totalDiscount,count,list=parseCursorForAverageDiscAndCount(cursor,operator,operand2)
-                    cursor.close
-                if operand1=='brand.name' and operator=='==':
-                    cursor=mongo.db.Dataset.find({operand1:operand2})
-                    for doc in cursor:
-                        regularPrice=doc['price']['regular_price']['value']
-                        offerPrice=doc['price']['offer_price']['value']
-                        if offerPrice<regularPrice:
-                            count+=1
-                            totalDiscount+=calculateDifferencePercent(regularPrice,offerPrice)
-                    if count>0:
-                        totalDiscount=totalDiscount/float(count)
-                    cursor.close
-            dict['discounted_products_count']=count
-            dict['avg_dicount']=totalDiscount
-            return (str(dict))
-        if queryType=='expensive_list':
-            if 'filters' in jsonRequest:
+        if 'query_type' in jsonRequest:
+            #fetching the request dictionary
+            queryType=jsonRequest['query_type']
+            #checking for request type discounted products list 
+            if queryType=='discounted_products_list':
+                set={""}
                 listFilter=jsonRequest['filters']# returns a list
+                #running all the scenarios
+                for x in range(len(listFilter)):
+                    #fetching the conditons
+                    operand1=listFilter[x]['operand1']
+                    operand2=listFilter[x]['operand2']
+                    operator=listFilter[x]['operator']
+                    #selecting the case to be used
+                    if operand1 == 'discount':    
+                        #getting the cursor to fetch the dicounted products,i.e., regular price > offer price and getting only the required fields
+                        cursor=mongo.db.Dataset.find({'$where':'this.price.regular_price.value > this.price.offer_price.value'},{'_id':1,'price.regular_price.value':1,'price.offer_price.value':1})
+                        #function call to parse and getting the required things
+                        tempSet=Utils.parseCursorForGettingObjectIdCondtions(cursor,operator,operand2)
+                        brand_set|=tempSet
+                        #closing the cursor
+                        cursor.close
+                    #checking if the second filter is used 
+                    elif operand1=='brand.name' and operator=='==':
+                        #quering the database to find the brand.name == operand2
+                        cursor=mongo.db.Dataset.find({'$where':'this.price.regular_price.value > this.price.offer_price.value',operand1:operand2},{'_id':1,'price.regular_price.value':1,'price.offer_price.value':1})
+                        disc_set|=Utils.parseCursorIntoSet(cursor)
+                        
+                        #closing the cursor
+                        cursor.close
+                #applying the results to the dictionary and breaking the flow of the program in such a manner that even if any wrong query is fired then the output will be empty set
+                set=Utils.mergeSet(brand_set,disc_set)
+                set.remove('')
+                dict['discounted_products_list']=list(set)
+                print(len(set))
+                return(str(dict))
+            if queryType=='discounted_products_count|avg_discount':
+                set={""}
+                dict
+                #creating variables for the count and discount
+                count=0
+                totalDiscount=0.0
+                listFilter=jsonRequest['filters']
                 for x in range(len(listFilter)):
                     operand1=listFilter[x]['operand1']
                     operand2=listFilter[x]['operand2']
                     operator=listFilter[x]['operator']
-                    if operand1=='brand.name' and operator=='==':
-                        cursor=mongo.db.Dataset.find({'$where':'this.price.basket_price.value > this.similar_products.meta.avg_price.basket','brand.name':operand2},{'_id':1,'price.regular_price.value':1,'price.offer_price.value':1})
-                        list=basicParseForIdList(cursor)
+                    if operand1 == 'discount':   
+                        #searching where regular price > offer price  
+                        cursor=mongo.db.Dataset.find({'$where':'this.price.regular_price.value > this.price.offer_price.value'},{'_id':1,'price.regular_price.value':1,'price.offer_price.value':1})
+                        #getting the ids of all the products required according to opperand supplied
+                        disc_set|=Utils.parseCursorForGettingObjectIdCondtions(cursor,operator,operand2)
+                        #closing the cursor
                         cursor.close
-            else:
-                cursor=mongo.db.Dataset.find({'$where':'this.price.basket_price.value > this.similar_products.meta.avg_price.basket'},{'_id':1})
-                list=basicParseForIdList(cursor)
-                cursor.close
-            dict={}
-            dict['expensive_list']=list
-            return str(dict)
-        if queryType=='competition_discount_diff_list':
-            if 'filters' in jsonRequest:
-                if len(jsonRequest['filters']) == 2:
+                    if operand1=='brand.name' and operator=='==':
+                        #searching where regular price > offer price and brand.name= operand 2
+                        cursor=mongo.db.Dataset.find({'$where':'this.price.regular_price.value > this.price.offer_price.value',operand1:operand2},{'_id':1,'price.regular_price.value':1,'price.offer_price.value':1})
+                        #getting the ids in a set 
+                        brand_set|=Utils.parseCursorIntoSet(cursor)
+                        #closing the cursor
+                        cursor.close
+                #and operation on the set
+                set=Utils.mergeSet(disc_set,brand_set)
+                if "" in set:
+                    set.remove("")
+                #iterating for all the ids in the set and calculating discount
+                for id in set:
+                    #finding data using id
+                    cursor=mongo.db.Dataset.find({'_id':ObjectId(id)})
+                    for doc in cursor:
+                        regularPrice=doc['price']['regular_price']['value']
+                        offerPrice=doc['price']['offer_price']['value']
+                        totalDiscount+=Utils.calculateDifferencePercent(regularPrice,offerPrice)
+                        print(totalDiscount)
+                    cursor.close
+                count=len(set)
+                #case where no ids are found
+                if count>0:
+                    print(totalDiscount)
+                    totalDiscount=totalDiscount/float(count)
+                #setting the values
+                dict['discounted_products_count']=len(set)
+                dict['avg_dicount']=totalDiscount
+                return (str(dict))
+            if queryType=='expensive_list':
+                set={""}
+                if 'filters' in jsonRequest:
                     listFilter=jsonRequest['filters']# returns a list
-                    operand01=listFilter[0]['operand1']
-                    operand02=listFilter[0]['operand2']
-                    operator0=listFilter[0]['operator']
-                    operand11=listFilter[1]['operand1']
-                    operand12=listFilter[1]['operand2']
-                    operator1=listFilter[1]['operator']
-                    if operand01=='discount_diff' and operand11=='competition' and operator1 == '==':
-                        cursor=mongo. db.Dataset.find()
-                        list=parseCursorCase4(cursor,operator0,operand02,operand12)
-                dict['competition_discount_diff_list']=list
-            return str(dict)
+                    #iterating
+                    for x in range(len(listFilter)):
+                        operand1=listFilter[x]['operand1']
+                        operand2=listFilter[x]['operand2']
+                        operator=listFilter[x]['operator']
+                        if operand1=='brand.name' and operator=='==':
+                            #finding docs with brand.name == operand2 
+                            cursor=mongo.db.Dataset.find({operand1:operand2})
+                            #finding the ids of products where basket_price > similar_product's basket prices
+                            set|=Utils.paresCursorForGettingIdForAllBrands(cursor)
+                            cursor.close
+                else:
+                    #finding all the ids of the products 
+                    cursor=mongo.db.Dataset.find({'$where':'/^/'})
+                    #finding the ids of products where basket_price > similar_product's basket prices for all the products
+                    set|=Utils.paresCursorForGettingIdForAllBrands(cursor)
+                    cursor.close
+                if "" in set:
+                    set.remove("")
+                #setting the data and sending it
+                dict['expensive_list']=list(set)
+                print(len(list(set)))
+                return str(dict)
+            if queryType=='competition_discount_diff_list':
+                set={""}
+                length=len(jsonRequest['filters'])
+                listFilter=jsonRequest['filters']# returns a list
+                #running all the scenarios
+                for x in range(len(listFilter)):
+                    operand1=listFilter[x]['operand1']
+                    operand2=listFilter[x]['operand2']
+                    operator=listFilter[x]['operator']
+                    if operand1=='discount_diff':
+                        cursor=mongo.db.Dataset.find()
+                        #querying according to the discount according to operator and operand2
+                        disc_set|=Utils.parsingCursorForDiscountCompareForParticularBrandHash(cursor,operator,operand2)
+                        cursor.close
+                    if operand1=='competition' and operator=='==':
+                        #querying according to discount for a particular brand hash 
+                        cursor=mongo.db.Dataset.find({'$where':'/^/'},{'similar_products.website_results.'+str(operand2)+'.knn_items':1,'_id':1})
+                        brand_set|=Utils.parseCursorForCompetitorProdId(cursor,operand2)
+                        cursor.close
+                #and the results for particular brands
+                set=Utils.mergeSet(brand_set,disc_set)
+                if "" in set:
+                    set.remove("")
+                # setting the results
+                dict['competition_discount_diff_list']=list(set)
+                return str(dict)
 
 # main driver function 
 if __name__ == '__main__': 
--- Utils.py
+++ Utils.py
@@ -1,57 +1,91 @@
 class Utils:
+    #list of all the compeititor's hashes
+    BRAND_ID_LIST=['5da94f4e6d97010001f81d72','5da94f270ffeca000172b12e','5d0cc7b68a66a100014acdb0','5da94ef80ffeca000172b12c','5da94e940ffeca000172b12a']
+    #method for calculating percacentage as (ar1-ag2)/(arg1)*100
     @staticmethod
     def calculateDifferencePercent(regularPrice,offerPrice):
         disPercent=(regularPrice-offerPrice)*100/regularPrice
         return disPercent
     
+    #method to parse cursor for getting ids based on disocunt percentage
     @staticmethod
-    def basicParseForIdList(cursor):
-        list=[]
-        for doc in cursor:
-            list.append(str(doc['_id']))
-        return list
-
-    @staticmethod
-    def parseCursorForAverageDiscAndCount(cursor,operator,operand2):
-        list=[]
+    def parseCursorForGettingObjectIdCondtions(cursor,operator,operand2):
+        set={""}
+        set.clear
         finalDis=0.0
         count=0
         for doc in cursor:
             regularPrice=doc['price']['regular_price']['value']
             offerPrice=doc['price']['offer_price']['value']
             discount=Utils.calculateDifferencePercent(regularPrice,offerPrice)
-            if operator =='>':
-                if discount > float(operand2):
-                    list.append(str(doc['_id']))
-                    finalDis+=discount
-                    count+=1
-            elif operator=='<':
-                if discount > float(operand2):
-                    list.append(str(doc['_id']))
-                    finalDis+=discount
-                    count+=1
-            elif operator=='==':
-                list.append(str(doc['_id']))
-                if discount > float(operand2):
-                    finalDis+=discount
-                    count+=1
-        return finalDis/count,count,list
+            s=str(discount) + operator + str(operand2)
+            if eval(s):
+                set.add(str(doc['_id']))
+        return set
 
+    #method to parse cursor to a set of object ids
     @staticmethod
-    def parseCursorCase4(cursor,operator,operand2,operand12):
-        list=[]
+    def parseCursorIntoSet(cursor):
+        set={""}
         for doc in cursor:
-            napBasketPrice=doc['price']['basket_price']['value']
-            comBasketPrice=doc['similar_products']['website_results'][str(operand12)]['meta']['avg_price']['basket']
-            discount=calculateDifferencePercent(napBasketPrice,comBasketPrice)
-            if operator =='>':
-                if discount > float(operand2):
-                    list.append(str(doc['_id']))
-            elif operator=='<':
-                if discount > float(operand2):
-                    list.append(str(doc['_id']))
-            elif operator=='==':
-                if discount == float(operand2):
-                    list.append(str(doc['_id']))
-        print(len(list))
-        return list
+            set.add(str(doc['_id']))
+        return set
+    
+    #method to parse cursor based on comparison with similar products in specific hashes
+    @staticmethod
+    def paresCursorForGettingIdForAllBrands(cursor):    
+        set={""}
+        for brand in Utils.BRAND_ID_LIST:
+            tempSet=Utils.paresCursorForSpecificBrand(cursor,brand)
+            set|=tempSet
+        return set
+
+    #comparing with specific hash of web
+    @staticmethod
+    def paresCursorForSpecificBrand(cursor,brandId):
+        set={""}
+        for doc in cursor:
+            napBasketPrice,comBasketPrice=Utils.getBasketPrices(doc,brandId)
+            if napBasketPrice > comBasketPrice:
+                set.add(str(doc['_id']))
+        # print(len(list))
+        return set 
+
+    #parsing ids for brands
+    @staticmethod
+    def parsingCursorForDiscountCompareForParticularBrandHash(cursor,operator,compareVal):
+        set={""}
+        for doc in cursor:
+            for brandHash in Utils.BRAND_ID_LIST:
+                napBasketPrice,comBasketPrice=Utils.getBasketPrices(doc,brandHash)
+                disc=Utils.calculateDifferencePercent(napBasketPrice,comBasketPrice)
+                s=str(disc)+operator+str(compareVal)
+                if eval(s):
+                    set.add(str(doc['_id']))
+        return set
+
+    
+    @staticmethod
+    def parseCursorForCompetitorProdId(cursor,brandHash):
+        set={""}
+        for doc in cursor:
+            array=doc['similar_products']['website_results'][str(brandHash)]['knn_items']
+            if len(array)>0:
+                set.add(str(doc['_id']))
+        return set
+
+    #getting basket prices
+    @staticmethod
+    def getBasketPrices(doc,brandId):
+        napBasketPrice=doc['price']['basket_price']['value']
+        comBasketPrice=doc['similar_products']['website_results'][str(brandId)]['meta']['avg_price']['basket']
+        return napBasketPrice,comBasketPrice
+
+    #merging the sets
+    @staticmethod
+    def mergeSet(set1,set2):
+        if len (set1)!=0 and len(set2)!=0:
+            return set1&set2
+        else:
+            return set1|set2
+    
